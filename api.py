4359244b732df3ab3f469f20ef1fb694fe6701ab
RameenEzwa <rameenezwajanjua2005@gmail.com>
2026-05-17 09:29:22 +0000

    fix api url

--- api.py
+++ api.py
@@ -46,5 +46,5 @@ def predict():
     except Exception as e:
         return jsonify({"success": False, "error": str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0', port=8080)
